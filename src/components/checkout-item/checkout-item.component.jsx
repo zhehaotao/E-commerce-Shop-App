821b21f99ff244cecedce8469f45f05ec7cce289
ZhehaoTao <zhehaotao@gmail.com>
2020-03-19 14:52:57 +0800

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -5,21 +5,36 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './checkout-item.styles.scss';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({cartItem: { imageUrl, price, name, quantity }}) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img alt='item' src={imageUrl} />
+const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
+  const { imageUrl, price, name, quantity } = cartItem;
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img alt='item' src={imageUrl} />
+      </div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+      </span>
+      <span className='price'>{price}</span>
+      <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
     </div>
-    <div className='name'>{name}</div>
-    <div className='quantity'>{quantity}</div>
-    <div className='price'>{price}</div>
-    <span className='remove-button'>&#10005;</span>
-  </div>
-)
+  )
+}
+
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 })
 
-export default connect(mapStateToProps)(CheckoutItem);
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, {...cartItemToAdd, quantity:1}];
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  if (cartItemToRemove.quantity > 1) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToRemove.id
+        ? {...cartItem, quantity:cartItem.quantity - 1}
+        : cartItem
+    )
+  }
+  return cartItems.filter(
+    cartItem => cartItem.id !== cartItemToRemove.id
+  )
+}
