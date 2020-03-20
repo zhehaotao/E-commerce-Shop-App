0aee0c7e05e4dfca83951bbc44f75610c52ef482
ZhehaoTao <zhehaotao@gmail.com>
2020-03-20 21:05:39 +0800

    added directory state and collection state into redux, added collection overview component to application

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/checkout' component={CheckoutPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)}  />
         </Switch>
       </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/collection-preview'
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
-    this.state = {
-      collections: SHOP_DATA  
-    }
-  }
-
-  render() {
-    const {collections} = this.state;
-    return (<div className='shop-page'>
-      {
-        collections.map(({id,...otherCollectionProps}) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))
-      }
-    </div>);
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionOverview />
+  </div>
+)
 
 export default ShopPage;
