5778a2c073e367d9facc9431b55f6dfdefcc8394
ZhehaoTao <zhehaotao@gmail.com>
2020-03-20 13:17:25 +0800

    fixed a user sign in bug

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component'
 
 import Header from './components/header/header.component'
-import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-import {setCurrentUser} from './redux/user/user.actions';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -62,7 +63,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser:setCurrentUser
+  currentUser:selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
