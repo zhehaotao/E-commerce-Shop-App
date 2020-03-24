d305ffd129214e2553f2c8dc36b8359d58949a5e
ZhehaoTao <zhehaotao@gmail.com>
2020-03-24 17:18:27 +0800

    added styled-components, converted homepage, header and custom button over to styled-components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import {auth} from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils'
 
-import {ReactComponent as Logo} from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
 const Header = ({currentUser, hidden}) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
+  <HeaderContainer>
+    <LogoContainer to='/'>
       <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>
         SHOP
-      </Link>
-      <Link className='option' to='/shop'>
+      </OptionLink>
+      <OptionLink to='/shop'>
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ?(
-        <div className = 'option' onClick = {() => auth.signOut()}>SIGN OUT</div>)
+        <OptionLink as='div' onClick = {() => auth.signOut()}>SIGN OUT</OptionLink>)
         :
-        (<Link className = 'option' to = '/signin'>SIGN IN</Link>
+        (<OptionLink to = '/signin'>SIGN IN</OptionLink>
         )}
-        <CartIcon />
-    </div>
+      <CartIcon />
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 )
 
 // state refers to root reducer, which is an object
